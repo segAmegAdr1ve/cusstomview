8bef020d90512fc8be9691555f23b66da2190b21
Sergej <serega7302@gmail.com>
2025-03-27 16:08:50 +0400

    moved logic from activity to fragment

--- app/src/main/java/com/example/cusstomview/MainActivity.kt
+++ app/src/main/java/com/example/cusstomview/MainActivity.kt
@@ -1,42 +1,19 @@
 package com.example.cusstomview
 
-import android.R.layout.simple_list_item_1
 import android.os.Bundle
-import android.view.MotionEvent
-import android.widget.ArrayAdapter
-import android.widget.ListView
 import androidx.activity.enableEdgeToEdge
-import androidx.activity.viewModels
-import androidx.appcompat.R.layout.support_simple_spinner_dropdown_item
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
-import androidx.lifecycle.lifecycleScope
-import androidx.recyclerview.widget.PagerSnapHelper
 import com.example.cusstomview.databinding.ActivityMainBinding
-import com.google.android.material.dialog.MaterialAlertDialogBuilder
-import kotlinx.coroutines.launch
-import java.time.LocalDate
-import java.time.LocalDateTime
-import java.time.LocalTime
-import java.time.Month
-import java.time.format.TextStyle
-import java.util.Locale
-
-private const val SELECTED_DATE_LIST_POSITION = "SELECTED_DATE_LIST_POSITION"
 
 class MainActivity : AppCompatActivity() {
     private var _binding: ActivityMainBinding? = null
-    private val binding: ActivityMainBinding by lazy { _binding!! }
-    private val viewModel: CalendarViewModel by viewModels()
+    private val binding get() = _binding!!
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         _binding = ActivityMainBinding.inflate(layoutInflater)
-
-        setupMonthPicker()
-        setupAdapter()
-
         setContentView(binding.root)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
@@ -44,82 +21,4 @@ class MainActivity : AppCompatActivity() {
             insets
         }
     }
-
-    private fun setupAdapter() {
-        val calendarAdapter = CalendarRecyclerViewAdapter()
-        binding.recyclerView.adapter = calendarAdapter
-        calendarAdapter.onSelectedDateChanged = { date: LocalDate ->
-            binding.dayTimelineView.selectedDateTime = LocalDateTime.of(date, LocalTime.now())
-        }
-
-        lifecycleScope.launch {
-            viewModel.currentMonth.collect { monthList ->
-                calendarAdapter.monthList = monthList
-                calendarAdapter.notifyDataSetChanged()
-            }
-        }
-
-        binding.recyclerView.scrollToPosition(2)
-        val pagerSnapHelper = PagerSnapHelper()
-        pagerSnapHelper.attachToRecyclerView(binding.recyclerView)
-    }
-
-    private fun setupMonthPicker() {
-        val locale = Locale.getDefault()
-        val monthList = Month.entries.map { month ->
-            month.getDisplayName(TextStyle.SHORT, locale)
-        }
-
-        val monthPickerAdapter = ArrayAdapter(
-            this,
-            support_simple_spinner_dropdown_item,
-            monthList
-        )
-        binding.monthSpinner.adapter = monthPickerAdapter
-        binding.monthSpinner.setSelection(viewModel.calendarHelper.selectedDate.month.value - 1)
-
-        binding.monthSpinner.setOnTouchListener { _, event ->
-            if (event.action == MotionEvent.ACTION_UP) {
-                showMonthPickerDialog(monthList)
-            }
-            true
-        }
-    }
-
-    private fun showMonthPickerDialog(monthList: List<String>) {
-        val adapter = ArrayAdapter(this, simple_list_item_1, monthList)
-        val listView = ListView(this)
-        listView.adapter = adapter
-        val alertDialog = MaterialAlertDialogBuilder(this)
-            .setTitle(getString(R.string.month_picker_title))
-            .setView(listView)
-            .show()
-
-        listView.setOnItemClickListener { _, _, position, _ ->
-            binding.monthSpinner.setSelection(position)
-            viewModel.onSelectedMonthChanged(Month.of(position + 1))
-            (binding.recyclerView.adapter as CalendarRecyclerViewAdapter).removeSelection()
-            alertDialog.dismiss()
-        }
-    }
-
-    override fun onSaveInstanceState(outState: Bundle) {
-        super.onSaveInstanceState(outState)
-        outState.putInt(
-            SELECTED_DATE_LIST_POSITION,
-            (binding.recyclerView.adapter as CalendarRecyclerViewAdapter).selectedDateListPosition
-        )
-    }
-
-    override fun onRestoreInstanceState(savedInstanceState: Bundle) {
-        super.onRestoreInstanceState(savedInstanceState)
-        (binding.recyclerView.adapter as CalendarRecyclerViewAdapter).selectedDateListPosition =
-            savedInstanceState.getInt(SELECTED_DATE_LIST_POSITION)
-    }
-
-    override fun onDestroy() {
-        super.onDestroy()
-        _binding = null
-    }
-
 }
